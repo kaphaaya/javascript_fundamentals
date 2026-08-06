4fed83b9a2e2a06254d74027be96a7b058e1c7de
kaphaaya <aziz.kafayat@gmail.com>
2026-08-06 21:27:44 +0100

    delete assignment folder for clean repo

--- ASSIGNMENT 1/practice.js
+++
@@ -1,8 +0,0 @@
-// Mini Project
-
-let budget = 5000
-let itemPrice = 1200
-
-console.log( budget > itemPrice )  && ( budget > 0 )
-
-//The item is affordable as it is lesser than the itemPrice and the budget is greater than 0.
